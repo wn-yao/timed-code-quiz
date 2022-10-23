493c60d3ad991d81bc2736093e7ec72da8b639bc
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-23 15:01:24 -0700

    worked with tutor and got the buttons for answers working

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,27 +16,32 @@ var showResult = document.querySelector(".hideResult");
 //Questions and it's answers 
 
 var questionsAndAnswers = [
-    {question: "Commonly used data types DO NOT include:",
-     options: ["strings", "booleans", "alerts", "numbers"],
-     CorrectAnswer: "alerts",
+  {
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    CorrectAnswer: "alerts",
   },
-    {quesion: "The condition in and if/else statement is enclosed within _____.",
-     options: ["quotes", "curly brackets", "parentheses", "square brackets"],
-     CorrectAnswer: "gparentheses",
+  {
+    quesion: "The condition in and if/else statement is enclosed within _____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    CorrectAnswer: "gparentheses",
   },
-    {question: "Array in JavaScript can be used to store ______.",
-     options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-     CorrectAnswer: "all of the above",
+  {
+    question: "Array in JavaScript can be used to store ______.",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    CorrectAnswer: "all of the above",
   },
-    {question: "String values must be enclosed within ____ when being assigned to variables.",
-     options: ["commas", "curly brackets", "quotes", "paratheses"],
-     CorrectAnswer: "quotes",
-  }, 
-    {question: "A very useful tool used during development and debugging for printint content to the debugger is:",
-     options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-     CorrectAnswer: "console.log",
+  {
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "paratheses"],
+    CorrectAnswer: "quotes",
   },
-  ];
+  {
+    question: "A very useful tool used during development and debugging for printint content to the debugger is:",
+    options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    CorrectAnswer: "console.log",
+  },
+];
 
 var currentQdisplay = 0;
 var totalTime = 90;
@@ -53,9 +58,9 @@ startQuizBtn.addEventListener("click", startQuiz);
 //submitScoreEl.addEventListener("click", storeScore);
 
 function startQuiz() {
-    startPageEl.remove();
-    startTimerTick();
-    displayQuestions();
+  startPageEl.remove();
+  startTimerTick();
+  displayQuestions();
 }
 
 
@@ -78,38 +83,45 @@ function startTimerTick() {
 var currentQuestionIndex = 0
 var correct = 0;
 
-//var answerOptions = document.createElement("button");
-//answerOptions.classList.add("btn");
-//answersEl.appendChild(answerOptions);
+function displayQuestions() {
+  quizboardEl.classList.remove("hideQuizBoard");
+  currentQ = questionsAndAnswers[currentQuestionIndex];
+  console.log(currentQ);
+  questionsEl.textContent = currentQ.question;
+  displayOptions();
+}
+//show second question when go to answer is selected 
 
+function displayOptions() {
+  for (i = 0; i < currentQ.options.length; i++) {
+    //optionsChoice = questionsAndAnswers[0];
+    var answerOptions = document.createElement("button");
+    answerOptions.classList.add("btn");
+    answerOptions.textContent = currentQ.options[i];
+    answersEl.appendChild(answerOptions);
+    //console.log(currentQ.options[i]);
+  }
+}
+//need to remove the first set of answer sets after making selection
+function selectAnswer() {
+  console.log(event.target.textContent );
+  //changing the currentQuestionIndex 
+  currentQuestionIndex++;
+  displayQuestions();
+}
 
-function displayQuestions () {
-    quizboardEl.classList.remove("hideQuizBoard");
-    currentQ = questionsAndAnswers[currentQuestionIndex];
-    console.log(currentQ);
-    questionsEl.textContent = currentQ.question;
-    displayOptions();
-    }
+answersEl.addEventListener("click", selectAnswer);
+//};
 
-function displayOptions () {
-  for (i = 0; i < questionsAndAnswers.length; i++) {
-  var optionsChoice = questionsAndAnswers[0]
-  console.log(optionsChoice);
-  answersEl.textContent = optionsChoice.options;
-}}
-
-    //answersEl.addEventListener("click", selectAnswer);
-    //};
-
-    //currentQuestion = questionsAndAnswers[currentIndex];
-    //quizboardEl.textContemt = questionsAndAnswers.question;
-    //answerOptions.classList.remove("visibility");
-    //console.log(displayQuestions)
+//currentQuestion = questionsAndAnswers[currentIndex];
+//quizboardEl.textContemt = questionsAndAnswers.question;
+//answerOptions.classList.remove("visibility");
+//console.log(displayQuestions)
 //}
 
 
 //answer correct?
 
 //resulst show after quize is done
-function quizGameOver() {}
+function quizGameOver() { }
 //
